544e61481b4c62306778717e4c91f8fceb71ca06
Nadia <enadialopez@gmail.com>
2022-07-13 23:33:08 -0300

    elimino un componente

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Home from './componentes/Home';
-import Rules from './componentes/Rules';
 import './App.css';
 import SinglePlayer from './componentes/SinglePlayer';
 import TwoPlayer from './componentes/TwoPlayer';
@@ -15,7 +14,6 @@ const App = () => {
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/singleplayer" element={<SinglePlayer/>}/>
             <Route exact path="/twoplayer" element={<TwoPlayer/>}/>
-            <Route exact path="/rules" element={<Rules/>}/>
           </Routes>
     </BrowserRouter>
   );
